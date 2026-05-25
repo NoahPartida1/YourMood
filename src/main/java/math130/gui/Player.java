1e3b346f13f57ee77131e9438151002cd35486c0
Noah <noahpartidahd@gmail.com>
2026-05-25 14:36:02 -0700

    Update Player.java

--- src/main/java/math130/gui/Player.java
+++ src/main/java/math130/gui/Player.java
@@ -2,9 +2,16 @@ package math130.gui;
 
 public class Player extends YourMoodCharacter {
 
+    /*
+    Unique variables for this child class of YourMoodCharacter
+     */
+
     private double critValue;
     private boolean crit;
-
+    
+    /*
+    Constructors for this class 
+     */
     public Player(int health, boolean alive, String emotion, int defence, double critValue, boolean crit) {
         super(health, alive, emotion, defence);
         this.crit = crit;
@@ -14,16 +21,13 @@ public class Player extends YourMoodCharacter {
     public Player() {
         this(10, false, "Happy", 10, 1.0, false);
     }
-
-    public void setCritValue(double hello) {
-        this.critValue = hello;
-    }
-
-    public boolean getCrit() {
-        return crit;
-    }
-
-
+    /*
+    This is the main method that is unique to the player subclass
+    in which the attack method is already created in the parent class 
+    YourMoodCharacter but now the idea of a 20% critical hit is implemented
+    and then super is used to call the original attack method while adding on
+    the condition if ult is true using the ult(boolean) method below
+     */
     public int attack(int max, int base, double ultimate) {
         ult(false);
         if ((int) (Math.random() * 10) + 1 >= 9) {
@@ -38,6 +42,12 @@ public class Player extends YourMoodCharacter {
         this.crit = yes;
     }
 
+    /*
+    These next two methods are the value constraints of an attack based on the
+    current emotion the player is in, the getMax value is one more than it's
+    supposed to be because of the Math.random value never being able to be 
+    rounded to the final value of for example 25 which is needed but would return 24
+     */
 
     public int getMax() {
         if (getEmotion().equals("Sad")) {
@@ -66,11 +76,14 @@ public class Player extends YourMoodCharacter {
             return 10;
         }
     }
-
-    public double getCritValue(){
-        return critValue;
-    }
-
+    
+    /*
+    These emotion getters will set the crit value of the player because that is the 
+    unique feature of the player subclass instead of healing for the monster class.
+    Not only do each of these methods change the crit value but also the defence
+    and String emotion of the player
+     */
+    
     public void getSad() {
         setEmotion("Sad");
         setDefence(100);
@@ -87,6 +100,21 @@ public class Player extends YourMoodCharacter {
         setEmotion("Angry");
         setDefence(0);
         setCritValue(5.0);
+    }
+
+/*
+normal setters and getters 
+ */
+    public void setCritValue(double hello) {
+        this.critValue = hello;
+    }
+
+    public boolean getCrit() {
+        return crit;
+    }
+
+    public double getCritValue(){
+        return critValue;
     }
 
 }
